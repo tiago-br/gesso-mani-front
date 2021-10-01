37188030343083a5766b8406f6379ee2b2b1c069
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-01 13:32:19 -0300

    arquivo-apiutils-criado

--- src/utils/api.util.js
+++ src/utils/api.util.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-require('dotenv').config()
+
 
 class Api {
     constructor(){
         this.api = axios.create({
             baseURL: "http://localhost:5000"
         })
-    this.api.interceptors.request({
+    this.api.interceptors.request.use({
 
     })
-    this.api.interceptors.response({
+    this.api.interceptors.response.use({
 
     })
     }
