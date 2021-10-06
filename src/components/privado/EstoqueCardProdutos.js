82ceba5206053cd27a292ae3038f430cd42ca2a4
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-06 09:24:17 -0300

    formulario do card do produto funcao Enable/disable

--- src/components/privado/EstoqueCardProdutos.js
+++ src/components/privado/EstoqueCardProdutos.js
@@ -16,16 +16,55 @@ class EstoqueCardProdutos extends Component {
         img_Url:this.props.img_Url,
         id:this.props._id,
         msg:"",
-        enableEdit:false
+        edit:{
+            disabled:"true"
+        },
+        load:false
+    }
+
+    componentDidMount=()=>{
+        this.setState({
+            load:true
+        })
+    }
+    handleChange = (e) =>{
+        const {name,value}= e.target
+
+        this.setState({
+            [name]:value
+        })
+    }
+    handleEditOn = () =>{
+            this.setState({
+                    edit:!this.state.edit
+            })
     }
     render() {
-        console.log(this.props)
         return (
             
             <div>
-                <div>
-
-                </div>
+                {this.state.load?
+                    <ContainerProduto>
+                        <FormCard>
+                        <fieldset disabled={this.state.edit}>
+                        <label>Produto</label>
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+                        <label>Quantidade em estoque</label>
+                        <input type="number" step="1" min="0" name="quantidade_em_estoque" value={this.state.quantidade_em_estoque} onChange={this.handleChange}></input>
+                        <label>Valor de venda</label>
+                        <input type="number" name="valor_de_venda" value={this.state.valor_de_venda}onChange={this.handleChange} min="0" step="0.01"></input>
+                        <label>Imagem URL </label>
+                        <input type="text" name="img_Url" value={this.state.img_Url} onChange={this.handleChange}/>
+                        <label>Descrição do produto</label>
+                        <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} rows="8" cols="70">{this.state.descricao}</textarea>
+                        <button>Atualizar Produto</button>
+                        </fieldset>
+                        </FormCard>
+                        <button onClick={this.handleEditOn}>Editar</button>
+                    </ContainerProduto>
+                :
+                <h1>Carregando ...</h1>
+                }
             </div>
         )
     }
