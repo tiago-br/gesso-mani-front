ddd1cf3bcd54efc1ae57e1fd1cce6b18e79a64d9
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-07 18:09:03 -0300

    botao de adicionar produto modificar

--- src/components/privado/EstoqueCardProdutos.js
+++ src/components/privado/EstoqueCardProdutos.js
@@ -26,7 +26,7 @@ class EstoqueCardProdutos extends Component {
 
     }
 
-    componentDidMount=async()=>{
+    componentDidMount(){
         this.setState({
             load:true
         })
@@ -162,12 +162,10 @@ class EstoqueCardProdutos extends Component {
     }
     render() {
         return (
-            
             <div>
                 {this.state.load?
                 <>
                     <form className="form-estoque-card">
-                       
                         {this.state.openDeletBar?
                         <div className="opened-delet-bar">
                         <p>Tem certeza que quer deletar o produto <span>{this.state.name}</span> do estoque?</p>
@@ -186,29 +184,21 @@ class EstoqueCardProdutos extends Component {
                             <div className="estoque-card-field-left">
                                 <label>Produto: </label>
                                 <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
-                          
-                                    
+            
                                 <label>Quantidade em estoque: </label>
                                 <input type="number" step="1" min="0" name="quantidade_em_estoque" value={this.state.quantidade_em_estoque} onChange={this.handleChange}></input>
-                            
-
-                      
                             
                                 <label>Valor de venda: </label>
                                 <input type="number" name="valor_de_venda" value={this.state.valor_de_venda}onChange={this.handleChange} min="0" step="0.01"></input>
                             </div>
                             <div className="estoque-card-field-right">
                                 <label>Imagem URL: </label>
-                                    <input type="text" name="img_Url" value={this.state.img_Url} onChange={this.handleChange}/>
-                                
-                            
-                        
+                                <input type="text" name="img_Url" value={this.state.img_Url} onChange={this.handleChange}/>                              
+                                                  
                                 <label>Descrição do produto:</label>
                                 <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} rows="5" cols="30">{this.state.descricao}</textarea>
                             </div>
-
                         </div>
-
 
                         <div className="estoque-card-bts">
                             <div>
@@ -241,8 +231,6 @@ class EstoqueCardProdutos extends Component {
                             </div>
                         }
                         </form>
-                        
-                        
                     </>
                 :
                 <h1>Carregando ...</h1>
--- src/components/privado/EstoqueNovoProduto.js
+++ src/components/privado/EstoqueNovoProduto.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import api from '../../utils/api.util'
+import './styles/EstoqueNovoProduto.css'
 
-
-const Titulo = styled.h1`
-    font-size: 2rem;
-    color: black;
-    text-align: center;
-    margin-top: 5vh;
-`
-
-const Form = styled.form`
-    width: 70vw;
-    height: 50vh;
-    font-size: 1.2rem;
-
-
-`
 
 class EstoqueNovoProduto extends Component {
     state={
@@ -25,7 +11,9 @@ class EstoqueNovoProduto extends Component {
         valor_de_venda:0,
         descricao:"",
         img_Url:"",
-        msg:""
+        msg:"",
+        msgSuccess:"",
+        appearMSG:true,
     }
     handleChange = (e) =>{
         const {name,value}= e.target
@@ -42,12 +30,15 @@ class EstoqueNovoProduto extends Component {
             valor_de_venda:this.state.valor_de_venda,
             descricao:this.state.descricao,
             img_Url:this.state.img_Url,
-            modificado_por:this.props.user,
+            modificado_por:localStorage.getItem('user'),
         }
         try {
             await api.postProduto(payload)
             this.setState({
-                msg:`Produto "${payload.name}" criado com sucesso`
+                msgSuccess:`Produto "${payload.name}" criado com sucesso`,
+                appearMSG:false,
+                msg:""
+
             })
         } catch (error) {
             const {data} = await api.getProduto()
@@ -55,7 +46,7 @@ class EstoqueNovoProduto extends Component {
             if(checkExist){
                 this.setState({
                 msg:`O produto "${payload.name}" já está registrado no estoque`
-
+             
                 })
             }else{this.setState({
                 msg:"Erro ao criar novo produto!"
@@ -70,27 +61,57 @@ class EstoqueNovoProduto extends Component {
                 img_Url:""
         })
     }
+    handleChangeForm=()=>{
+
+        this.setState({
+            msgSuccess:"",
+            msg:"",
+            appearMSG:true
+        })
+    }
+    
 
 
     render() {
         console.log(this.state.descricao)
         return (
-            <div>
-                <Titulo>Novo Produto</Titulo>
-                <Form>
-                    <label>Nome do produto</label>
+            <div className="estoque-novo-produto-container">
+                <h1>Novo Produto</h1>
+                {this.state.appearMSG?
+                    <form>
+                    <div>
+                    <label>Nome do produto:</label>
                     <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+                    </div>
+                    <div>
                     <label>Quantidade em estoque</label>
                     <input type="number" step="1" min="1" name="quantidade_em_estoque" value={this.state.quantidade_em_estoque} onChange={this.handleChange}></input>
-                    <label>Valor de venda</label>
+                    </div>
+                    <div>
+                    <label>Valor de venda:</label>
                     <input type="number" name="valor_de_venda" value={this.state.valor_de_venda}onChange={this.handleChange} min="0" step="0.01"></input>
+                    </div>
+                    <div>
                     <label>Imagem URL </label>
                     <input type="text" name="img_Url" value={this.state.img_Url} onChange={this.handleChange}/>
-                    <label>Descrição do produto</label>
-                    <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} rows="8" cols="70">{this.state.descricao}</textarea>
-                    <button onClick={this.handleSubmit}>Criar Novo Produto</button>
-                </Form>
-                <p>{this.state.msg}</p>
+                    </div>
+                    <div>
+                        <div>
+                        <label>Descrição do produto</label>
+                        </div>
+                    <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} rows="4" cols="40">{this.state.descricao}</textarea>
+                    </div>
+                    <div>
+                    <button onClick={this.handleSubmit}>Criar novo produto</button>
+                    </div>
+                    <p>{this.state.msg}</p>
+                    </form>
+                    :
+                    <div className="div-msg-success-novo-produto">
+                    <h1>{this.state.msgSuccess}</h1>
+                    <button onClick={this.handleChangeForm}>Criar outro produto</button>
+                    </div>
+                    }
             </div>
         )
     }
