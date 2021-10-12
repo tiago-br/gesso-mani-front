25183d391485ce2335868ffa5c4c71a84092b663
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-12 03:42:43 -0300

    FuncionarioPage adicionado a funcao de excluir os usuarios

--- src/components/privado/funcionarios/GerenciarFuncionarios.js
+++ src/components/privado/funcionarios/GerenciarFuncionarios.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
+import api from '../../../utils/api.util'
+import UsersCard from './UsersCard'
 
 class GerenciarFuncionarios extends Component {
     state={
-        load:false
+        load:false,
+        users:[]
     }
-    componentDidMount(){
-        
+    componentDidMount = async() =>{
+        const {data} = await api.getUsers()
+
+        this.setState({
+            users:data,
+            load:true
+        })
+    }
+    handleDeleteUser = async(id) =>{
+        try {
+           await api.deleteUsers(id)
+           const {data} = await api.getUsers()
+           this.setState({
+               users:data
+           })
+        } catch (error) {
+        }
     }
     render() {
+        console.log(this.state.users)
         return (
             <div>
                 {this.state.load?
                     <section>
-                        
+                        {
+                            this.state.users.map(user=>
+                            <UsersCard delete={this.handleDeleteUser} id={user._id} user={user.username}/>
+                            )
+                        }
                     </section>
                     :
                     null  
