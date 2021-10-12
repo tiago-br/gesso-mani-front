a660109c3150e68de1ed07cba3e9961036d73821
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-12 01:32:03 -0300

    Na navbar foi adicionado o nome do usuario que esta logado junto a um botao de logout

--- src/components/privado/NavbarUser.js
+++ src/components/privado/NavbarUser.js
@@ -14,7 +14,7 @@ const NavBar = styled.nav`
     a{
         text-decoration: none;
         color:black;
-        font-size: 1.3rem;
+        font-size: 1.1rem;
     }
     a:hover{
         text-decoration: none;
@@ -23,17 +23,48 @@ const NavBar = styled.nav`
     }
 `
 
+const Div = styled.div`
+
+    p{
+        width: 10vw;
+        text-align: center;
+        font-size: 1.1rem;
+    }
+
+    button{
+        border: none;
+        outline: none;
+        cursor: pointer;
+        background-color: inherit;
+    }
+    button:hover{
+        color: white;
+    }
+`
+
 class NavbarUser extends Component {
+    state={
+        user:localStorage.getItem('user')
+    }
+
+    handleLogout = () =>{
+        localStorage.clear()
+        window.location.reload()
+    }
     render() {
         return (
             
             <NavBar>
-                <div>Logo</div>
                 <NavLink to={"/sistema/vendas"}>Vendas</NavLink>
                 <NavLink to={"/sistema/orçamento"}>Orçamento</NavLink>
                 <NavLink to={"/sistema/estoque"}>Estoque</NavLink>
                 <NavLink to={"/sistema/faturamento"}>Faturamento</NavLink>
                 <NavLink to={"/sistema/novofuncionario"}>Novo Funcionário</NavLink>
+                <Div>
+                    <p>{this.state.user} / <button onClick={this.handleLogout}>Logout</button> </p>
+                    {/* <Button>Logout</Button> */}
+                </Div>
+                
             </NavBar>
         )
     }
