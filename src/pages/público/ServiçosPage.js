7fecaef021da60609c00b599ee363e0656a4aa04
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-10-13 14:42:34 -0300

    pages ContatoPAge e ProdutosPage criadas com sucesso

---
+++ src/pages/público/ServiçosPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ServiçosPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Serviços Page</h1>
+            </div>
+        )
+    }
+}
+
+export default ServiçosPage
---
+++ src/pages/público/SobrePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SobrePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SobrePage</h1>
+            </div>
+        )
+    }
+}
+
+export default SobrePage
