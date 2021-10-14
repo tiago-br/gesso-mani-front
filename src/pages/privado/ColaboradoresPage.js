03a68419863baf2a71f91f743ef5e09272728eba
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-10-14 18:07:11 -0300

    Novas Rotas, e novos components

---
+++ src/pages/privado/ColaboradoresPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import NavbarUser from '../../components/privado/NavbarUser'
+
+
+ class ColaboradoresPage extends Component {
+    render() {
+        return (
+            <div>
+                <NavbarUser/>
+                ColaboradoresPage
+            </div>
+        )
+    }
+}
+
+export default ColaboradoresPage
