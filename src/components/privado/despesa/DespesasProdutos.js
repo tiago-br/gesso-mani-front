8f90a98c7659f119a376c2d7f7130a09ff2061d6
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-20 10:43:39 -0300

    posicoes botoes colaboradores arrumado

--- src/components/privado/despesa/DespesasProdutos.js
+++ src/components/privado/despesa/DespesasProdutos.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import apiUtil from '../../../utils/api.util'
 
 export class DespesasProdutos extends Component {
+    state={
+        produtos:[],
+        load:false
+    }
+    componentDidMount =async() =>{
+        const produtos = await apiUtil.getProduto()
+        this.setState({
+            load:true,
+            produtos
+        })
+    }
     render() {
         return (
             <div>
-                Ola
+                {this.state.load?
+                
+                <div>test</div>
+                :
+                <h2>Carregando...</h2>}
             </div>
         )
     }
